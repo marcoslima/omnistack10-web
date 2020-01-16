6628e6373cd9bc96af7a5181ae5935f29481d731
Marcos Lima <marcos.j.s.lima@gmail.com>
2020-01-15 23:22:17 -0300

    pré implementação do hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import "./global.css";
+import "./sidebar.css";
+import "./Main.css";
+import "./App.css";
+
+navigator.geolocation.getCurrentPosition()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+          <button type='submit'>Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/1447643?s=460&v=4" alt="Marcos Lima"/>
+                   <div className="user-info">
+                     <strong>Marcos Lima</strong>
+                     <span>C++, Python, ReactJS</span>
+                   </div>
+            </header>
+            <p>Líder técnico na Storm Group</p>
+            <a href="https://github.com/marcoslima">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/1447643?s=460&v=4" alt="Marcos Lima"/>
+                   <div className="user-info">
+                     <strong>Marcos Lima</strong>
+                     <span>C++, Python, ReactJS</span>
+                   </div>
+            </header>
+            <p>Líder técnico na Storm Group</p>
+            <a href="https://github.com/marcoslima">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/1447643?s=460&v=4" alt="Marcos Lima"/>
+                   <div className="user-info">
+                     <strong>Marcos Lima</strong>
+                     <span>C++, Python, ReactJS</span>
+                   </div>
+            </header>
+            <p>Líder técnico na Storm Group</p>
+            <a href="https://github.com/marcoslima">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/1447643?s=460&v=4" alt="Marcos Lima"/>
+                   <div className="user-info">
+                     <strong>Marcos Lima</strong>
+                     <span>C++, Python, ReactJS</span>
+                   </div>
+            </header>
+            <p>Líder técnico na Storm Group</p>
+            <a href="https://github.com/marcoslima">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
